d61fddeb7958cf1c7f4f214bdc72e09c04583fab
Kallol Borah <kallol.borah@gmail.com>
2021-12-29 22:41:30 +0530

    updated custodial wallet creator and participant IDs

--- contracts/IVault.sol
+++ contracts/IVault.sol
@@ -12,27 +12,27 @@ interface IVault {
         bytes32 transaction;
         uint256 datetime;
         uint256 amount;
-        address[] cosigners;
+        uint256[] cosigners;
         address from;
         address to;
     }
 
-    function createVault(address _vault) external;
+    function createVault(uint256 _creator) external;
 
-    function addParticipant(address _vault, address _participant) external;
+    function addParticipant(uint256 _creator, uint256 _participant) external;
 
-    function removeParticipant(address _vault, address _participant) external;
+    function removeParticipant(address _vault, uint256 _creator, uint256 _participant) external;
 
-    function confirmParticipant(address _vault, bytes32 _shard) external;
+    function confirmParticipant(address _vault, uint256 _participant, bytes32 _shard, bytes32 _pin) external;
 
-    function defineQuorum(address _vault, uint256 _minParticipants) external;
+    function defineQuorum(address _vault, uint256 _creator, uint256 _minParticipants) external;
 
-    function signTransaction(address _vault, bytes32 _txHash, address _from, address _to, uint256 _amount) external;
+    function signTransaction(address _vault, uint256 _participant, bytes32 _txHash, address _from, address _to, uint256 _amount) external;
 
     function checkQuorum(address _vault, bytes32 _txHash) external view returns(bool);
 
     function getTransactions(address _vault) external view returns(transaction[] memory);
 
-    function getShard(address _vault) external view returns(bytes32);
+    function getShard(address _vault, uint256 _participant) external view returns(bytes32);
 
 }
--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -10,10 +10,13 @@ import "./IVault.sol";
 contract Vault is IVault {
 
     //mapping vault address (public key) to participants in the vault
-    mapping(address => address[]) private vaults;
+    mapping(address => uint256[]) private vaults;
+
+    //mapping vault address to participants to their pin
+    mapping(address => mapping(uint256 => bytes32)) private pins;
 
     //mapping vault address to participants to their key shard
-    mapping(address => mapping(address => bytes32)) private shards;
+    mapping(address => mapping(uint256 => bytes32)) private shards;
 
     //mapping vault address to number of minimum co-signers required for quorum
     mapping(address => uint256) private quorum;
@@ -21,52 +24,59 @@ contract Vault is IVault {
     //mapping vault address to transactions
     mapping(address => transaction[]) private transactions;
 
-    function createVault(address _vault) external {
-        require(vaults[_vault].length==0);
-        vaults[_vault].push(msg.sender);
+    //event that helps notify a participant to confirm PIN and shard
+    event promptNewParticipant(address vault, uint256 participant);
+
+    function createVault(uint256 _creator) external {
+        require(vaults[msg.sender].length==0);
+        vaults[msg.sender].push(_creator);
     }
 
-    function addParticipant(address _vault, address _participant) external {
-        require(vaults[_vault].length>0);
-        require(vaults[_vault][0]==msg.sender);
-        vaults[_vault].push(_participant);
+    function addParticipant(uint256 _creator, uint256 _participant) external {
+        require(vaults[msg.sender].length>0);
+        require(vaults[msg.sender][0]==_creator);
+        vaults[msg.sender].push(_participant);
+        emit promptNewParticipant(msg.sender, _participant);
     }
 
-    function removeParticipant(address _vault, address _participant) external {
+    function removeParticipant(address _vault, uint256 _creator, uint256 _participant) external {
         require(vaults[_vault].length>0);
-        require(vaults[_vault][0]==msg.sender);
+        require(vaults[_vault][0]==_creator);
         for(uint256 i=0; i<vaults[_vault].length; i++){
             if(vaults[_vault][i]==_participant)
                 delete vaults[_vault][i];
+                delete shards[_vault][_participant];
+                delete pins[_vault][_participant];
         }
     } 
 
-    function confirmParticipant(address _vault, bytes32 _shard) external {
+    function confirmParticipant(address _vault, uint256 _participant, bytes32 _shard, bytes32 _pin) external {
         require(vaults[_vault].length>0);
         for(uint256 i=0; i<vaults[_vault].length; i++){
-            if(vaults[_vault][i]==msg.sender)
-                shards[_vault][msg.sender] = _shard;
+            if(vaults[_vault][i]==_participant)
+                shards[_vault][_participant] = _shard;
+                pins[_vault][_participant] = _pin;
         }
     }
 
-    function defineQuorum(address _vault, uint256 _minParticipants) external {
+    function defineQuorum(address _vault, uint256 _creator, uint256 _minParticipants) external {
         require(vaults[_vault].length>0);
-        require(vaults[_vault][0]==msg.sender);
+        require(vaults[_vault][0]==_creator);
         quorum[_vault] = _minParticipants;
     }
 
-    function signTransaction(address _vault, bytes32 _txHash, address _from, address _to, uint256 _amount) external {
-        require(shards[_vault][msg.sender]!="");
+    function signTransaction(address _vault, uint256 _participant, bytes32 _txHash, address _from, address _to, uint256 _amount) external {
+        require(shards[_vault][_participant]!="");
         bool found = false;
         for(uint i=0; i<transactions[_vault].length; i++){
             if(transactions[_vault][i].transaction==_txHash){
-                transactions[_vault][i].cosigners.push(msg.sender);
+                transactions[_vault][i].cosigners.push(_participant);
                 found = true;
             }
         }
         if(!found){
-            address[] memory cs;
-            cs[0] = msg.sender;
+            uint256[] memory cs;
+            cs[0] = _participant;
             transaction memory currentTx = transaction({
                 transaction : _txHash,
                 datetime : now,
@@ -79,12 +89,12 @@ contract Vault is IVault {
         }
     }
 
-    function checkQuorum(address _vault, bytes32 _txHash) external view returns(bool){
-        require(shards[_vault][msg.sender]!="");
+    function checkQuorum(address _vault, uint256 _participant, bytes32 _txHash) external view returns(bool){
+        require(shards[_vault][_participant]!="");
         bool found = false;
         for(uint i=0; i<transactions[_vault].length; i++){
             if(transactions[_vault][i].transaction==_txHash){
-                if(transactions[_vault][i].cosigners.length == quorum[_vault])
+                if(transactions[_vault][i].cosigners.length >= quorum[_vault])
                     found = true;
             }
         }
@@ -98,8 +108,8 @@ contract Vault is IVault {
         return transactions[_vault];
     }
 
-    function getShard(address _vault) external view returns(bytes32){
-        return shards[_vault][msg.sender];
+    function getShard(address _vault, uint256 _participant) external view returns(bytes32){
+        return shards[_vault][_participant];
     }
 
 }
